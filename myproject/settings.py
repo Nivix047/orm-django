7a131f629f0cf9030a142d391cc3461d0fb50c5c
Daniel Xu <xudh47@gmail.com>
2023-09-14 22:04:00 -0700

    Added comments to myproject/settings.py & users/views.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -1,19 +1,27 @@
 from pathlib import Path
 from decouple import config
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Define the base directory of the Django project.
+# This is used for constructing absolute paths to various resources.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Fetch the secret key from environment or a .env file.
+# This key is used for cryptographic signing in Django.
 SECRET_KEY = config("SECRET_KEY")
 
+# Fetch the DEBUG setting from environment or a .env file.
+# It determines if the application is in debug mode (not recommended for production).
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
+# A list of IP addresses or hostnames which can be used to access the application.
+# Should be updated for production to restrict access.
 ALLOWED_HOSTS = []
 
 
 # Application definition
 
+# List of applications (both built-in and custom) that are enabled for this Django project.
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -26,6 +34,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
 ]
 
+# List of middleware classes that process request and response globally.
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -36,8 +45,10 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+# The main URL configuration for the project.
 ROOT_URLCONF = "myproject.urls"
 
+# Configuration for Django's templating system.
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -54,12 +65,13 @@ TEMPLATES = [
     },
 ]
 
+# WSGI configuration for serving the Django application.
 WSGI_APPLICATION = "myproject.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+# Here, PostgreSQL is used as the database with credentials fetched from environment or .env file.
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -71,8 +83,8 @@ DATABASES = {
     }
 }
 
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+# Validators for user passwords.
+# They ensure passwords adhere to certain security standards.
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -90,30 +102,27 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
+# Settings for internationalization and localization.
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
-
 TIME_ZONE = "UTC"
-
 USE_I18N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
+# Settings for static files (CSS, JS, images).
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
+# Default type for auto-generated primary key fields.
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+# Specifies a custom user model named "CustomUser" in the "users" app.
 AUTH_USER_MODEL = "users.CustomUser"
 
+# Configuration for Django Rest Framework (DRF).
+# It specifies authentication, parsing, and permission settings for DRF.
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication",
@@ -126,6 +135,8 @@ REST_FRAMEWORK = {
     ),
 }
 
+# Configuration for logging in Django.
+# Currently, logs of level DEBUG are printed to the console for 'django' and 'users' modules.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
--- users/views.py
+++ users/views.py
@@ -11,37 +11,69 @@ from rest_framework.authentication import TokenAuthentication
 from .models import CustomUser
 from .serializers import CustomUserSerializer
 
+# Initialize the logger for this module
 logger = logging.getLogger(__name__)
+
+# Define a ViewSet for the CustomUser model.
+# ViewSets are a type of class-based view provided by DRF
+# that provide CRUD operations by default.
 
 
 class CustomUserViewSet(viewsets.ModelViewSet):
+    # Define the default queryset that this ViewSet will operate on.
     queryset = CustomUser.objects.all()
+
+    # Define the serializer that this ViewSet will use to convert
+    # between Django models and JSON representations.
     serializer_class = CustomUserSerializer
 
+    # Define a custom action for this ViewSet. This action will handle user registration.
     @action(detail=False, methods=['POST'], permission_classes=[AllowAny])
     def register(self, request):
+        # Log an informational message whenever this endpoint is accessed.
         logger.info("Register endpoint accessed.")
+
+        # Deserialize the incoming request data into a CustomUser object.
         serializer = self.get_serializer(data=request.data)
+
+        # Validate the deserialized data.
         if serializer.is_valid():
+            # If valid, save the new user to the database.
             user = serializer.save()
+
+            # Create a token for the new user or get the existing one.
             token, created = Token.objects.get_or_create(user=user)
+
+            # Return the token and user ID as a response.
             return Response({'token': token.key, 'user_id': user.pk}, status=status.HTTP_201_CREATED)
+
+        # If the data is not valid, return the errors.
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Define a function-based view to handle user login.
 
 
 @api_view(["POST"])
 @permission_classes([AllowAny])
 def login_view(request):
+    # Extract username and password from the request data.
     username = request.data.get("username")
     password = request.data.get("password")
 
+    # Check if both username and password are provided.
     if not username or not password:
         return Response({"error": "Username and password are required"}, status=400)
 
+    # Try to authenticate the user with the provided credentials.
     user = authenticate(username=username, password=password)
 
+    # If the user is authenticated successfully...
     if user:
+        # Create a token for the user or get the existing one.
         token, created = Token.objects.get_or_create(user=user)
+
+        # Return the token and user ID as a response.
         return Response({"token": token.key, "user_id": user.pk})
+    # If authentication fails, return an error message.
     else:
         return Response({"error": "Invalid Credentials"}, status=401)
