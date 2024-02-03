6d74a3fcfd1f25ec09b49f125b286607a35dd6e0
Daniel Xu <xudh47@gmail.com>
2024-02-02 18:32:08 -0800

    update settings.py and docker-compose.yml to debug the localhost issue / not debugged yet

--- myproject/settings.py
+++ myproject/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
         'NAME': config("DATABASE_NAME"),
         'USER': config("DATABASE_USER"),
         'PASSWORD': config("DATABASE_PASSWORD"),
-        'HOST': config("DATABASE_HOST"),
-        'PORT': config("DATABASE_PORT"),
+        'HOST': config("DATABASE_HOST", default='db'),
+        'PORT': config("DATABASE_PORT", default='5432'),
     }
 }
 
